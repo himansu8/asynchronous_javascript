09e22c02f8d1a9420c08a2a74dacd91a999bbdd2
himansu8 <himansusekharsahu50@gmail.com>
2023-10-26 14:05:22 +0530

    asynchronous

---
+++ fs_project/example.js
@@ -0,0 +1,21 @@
+import fs from 'node:fs';
+
+function writeToAFile(){
+    // let data = "hello world";
+    let data = [1,2,3];
+fs.writeFile('sample.txt', JSON.stringify(data) ,(err)=>{
+    if(err) throw err;
+    console.log("Data has ben written to the file successfully");
+})
+}
+writeToAFile();
+
+
+function readFromFile(){
+    fs.readFile('sample.txt',(err,data)=>{
+        if(err) throw err
+        data = String(data);
+        console.log(data);
+    })
+}
+// readFromFile()
