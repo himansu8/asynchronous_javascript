3de29d21654b06e6f6be51f2251ee8863081f336
himansu8 <himansusekharsahu50@gmail.com>
2023-10-26 15:54:13 +0530

    JSON.stringfy & JSON.parse, readFromFile

--- fs_project/2.js
+++ fs_project/2.js
@@ -1,21 +1,24 @@
-import fs from 'node:fs';
+import fs, { read } from 'node:fs';
+import readlineSync from 'readline-sync';
 
 function writeToAFile(){
-    // let data = "hello world";
+//  let data = readlineSync.question("write some data to the file sample2.tex : ");
+    // let data = String(1)   //number to string
     let data = [1,2,3];
-fs.writeFile('sample.txt', JSON.stringify(data) ,(err)=>{
+fs.writeFile('sample2.txt', JSON.stringify(data) ,(err)=>{
     if(err) throw err;
     console.log("Data has ben written to the file successfully");
 })
 }
-writeToAFile();
+// writeToAFile();
 
 
 function readFromFile(){
-    fs.readFile('sample.txt',(err,data)=>{
+    fs.readFile('sample2.txt',(err,data)=>{
         if(err) throw err
         data = String(data);
+        // data =JSON.parse(data);
         console.log(data);
     })
 }
-// readFromFile() 
+readFromFile() 
