87a9a65e5caeac58222aff3d89914f69665d69b4
himansu8 <himansusekharsahu50@gmail.com>
2023-11-06 23:57:20 +0530

    encoding and decoding Base64

--- base_64/decoding.js
+++ base_64/decoding.js
@@ -1,13 +1,13 @@
 import readlineSync from "readline-sync";
 function decoding(){
     const inputString = readlineSync.question("Enter the decode word : ");
-//     const splitString = inputString.split("");
-//     //console.log(splitString);
-// const base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-// const base64Value = splitString.map((ele)=>ele(base64));
-// console.log(base64Value);
-var decodedString = atob(inputString);
-console.log(`base64 decoding of "${inputString}" is "${decodedString}"`);
+    const splitString = inputString.split("");
+    //console.log(splitString);
+const base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+const base64Value = splitString.map((ele)=>base64.indexOf(ele));
+console.log(base64Value);
+// var decodedString = atob(inputString);
+// console.log(`base64 decoding of "${inputString}" is "${decodedString}"`);
 
 }
 decoding() 
